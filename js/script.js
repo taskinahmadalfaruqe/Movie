1d8d343cf0c26af2955b514fb1bb40f82c145986
TASKIN AHMAD AL FARUQE <119452412+taskinahmadalfaruqe@users.noreply.github.com>
2023-03-15 00:55:56 +0600

    Movie

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('.slider_main').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        arrows:false,
+        centerMode: true,
+        centerPadding:"0150px",
+        focusOnSelect: true,
+      });
+});
